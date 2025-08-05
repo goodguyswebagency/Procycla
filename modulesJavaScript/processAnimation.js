b1587c92ca4939d2b2191e07c43c8bdbfc9c35c8
goodguyswebagency <github@goodguys.se>
2025-08-05 15:48:35 +0200

    Update processAnimation.js

--- modulesJavaScript/processAnimation.js
+++ modulesJavaScript/processAnimation.js
@@ -3,27 +3,45 @@
 /**************************/
 
 export default function processAnimation() {
-   let startValue;
-   let endValue;
+   let startValueIn;
+   let endValueIn;
+   let startValueOut;
+   let endValueOut;
 
    if (window.innerWidth > 991) {
-      startValue = "top 75%";
-      endValue = "top 55%";
+      startValueIn = "top 75%";
+      endValueIn = "top 55%";
+      startValueOut = "top 35%";
+      endValueOut = "top 0%";
    } else {
-      startValue = "top 85%";
-      endValue = "top 65%";
+      startValueIn = "top 85%";
+      endValueIn = "top 65%";
+      startValueOut = "top 35%";
+      endValueOut = "top 10%";
    }
 
    gsap.utils.toArray(".process_usp_wrapper").forEach((el) => {
-      gsap.from(el, {
-         opacity: 0,
+      gsap.set(el, { opacity: 0 });
+
+      const fadeOutEnabled = el.getAttribute("data-fade-out") !== "false";
+
+      const tl = gsap.timeline({
          scrollTrigger: {
             trigger: el,
-            start: startValue,
-            end: endValue,
+            start: startValueIn,
+            end: endValueOut,
             scrub: true,
-            markers: false,
+            markers: true,
          },
       });
+
+      tl.to(el, { opacity: 1, duration: 1, ease: "none" }).to(el, {
+         opacity: 1,
+         duration: 2.25,
+         ease: "none",
+      });
+      if (fadeOutEnabled) {
+         tl.to(el, { opacity: 0, duration: 1, ease: "none" });
+      }
    });
 }
